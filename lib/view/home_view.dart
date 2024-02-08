e55b1ea7af5340e2fe0d79bfd40bf6575cc74ab3
erdmgfb1907 <erdemyalcin571@gmail.com>
2024-02-08 19:56:06 +0300

    home view designing

--- lib/view/home_view.dart
+++ lib/view/home_view.dart
@@ -27,76 +27,86 @@ class _HomePageState extends State<HomePage> {
           Center(
             child: Text('Welcomess ${widget.username}'),
           ),
-          Center(
-            child: Image.asset(
-              "resim/resim2/LittleBite.png",
-              width: 300,
-              height: 150,),
-          ),
-           Row(
-             children: [
-               GestureDetector(
-                 onTap: (){
-                   print('TODO');
-                 },
-                 child: Image.asset(
-                   "resim/resim2/Group 1686550929.png",
-                   width: 235,
-                   height: 235,),
-               ),
-               GestureDetector(
-                 onTap: (){
-                   print('Food');
-                 },
-                 child: Image.asset(
-                   "resim/resim2/Group 1686550930.png",
-                   width:235,
-                   height: 235,),
-               ),
-
-             ],
-           ),
-          SizedBox(
-            height: 50,
-          ),
           Row(
             children: [
-              Center(
-                child: Image.asset(
-                  "resim/resim2/Group 1686550933.png",
-                  width: 235,
-                  height: 235,),
+
+              SizedBox(
+                width: 180,
+                height: 180,
+                child: Card(
+                  color: const Color.fromRGBO(141, 174, 74, 1.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: 50,
+
+
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 15.0), // Dikey yönde 8 piksel boşluk ekler
+                        child: Row(),
+                      ),
+
+
+                      Text(
+                        "Search",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
-              Center(
-                child: Image.asset(
-                  "resim/resim2/Group 1686550936.png",
-                  width: 250,
-                  height: 235,),
+              SizedBox(
+                width: 180,
+                height: 180,
+                child: Card(
+                  color: const Color.fromRGBO(244, 198, 29, 1.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.fact_check,
+                        color: Colors.white,
+                        size: 70
+                        ,
+                      ),
+                      Text(
+                        "To Do List",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               ),
 
 
-            ],
+                 ],
+               ),
+         Row(
+           children: [
+           Text('A healthy dieta better life',style: TextStyle(
+           color: Colors.black,
+    fontSize: 30,
+    fontWeight: FontWeight.bold
+    ),)
 
-          ),
-          Row(
-            children: [
-          Center(
-            child: Image.asset(
-              "resim/resim2/Group 1686550937.png",
-              width: 235,
-              height: 235,),
-          ),
-
-
-          ],
-
-          ),
-
-          SizedBox(
-            height: 20,
-          ),
+         ],)
         ],
       ),
+
+
+
 
       bottomNavigationBar: BottomNavigationBar(
       items: <BottomNavigationBarItem>[
